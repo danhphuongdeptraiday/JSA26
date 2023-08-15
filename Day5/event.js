3669e5d2b9d2cd14d56e27ab069498aee0ce13c6
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-08-16 01:34:43 +0700

    new update

--- Day5/event.js
+++ Day5/event.js
@@ -1,9 +1,53 @@
-function clickMe() {
+// Click me btn
+let clickMeBtn = document.getElementById("clickMeBtn");
+console.log(clickMeBtn);
+clickMeBtn.addEventListener("click", function () {
   console.log("hello my friend");
-}
+});
 
+// function clickMe() {
+// }
+
+// Count
+let countBtn = document.getElementById("countBtn");
 let i = 0;
-function count() {
-  i = i + 1;
-  console.log("Lần bấm thứ " + i);
-}
+countBtn.addEventListener("click", function () {
+  if (i < 10) {
+    i = i + 1;
+    let newH1 = document.createElement("h1");
+    newH1.innerText = "Lần bấm thứ " + i;
+    document.body.appendChild(newH1);
+  } else {
+    alert("Full");
+  }
+});
+
+//TurnOn/Off
+let turnOn = document.getElementById("turnOn");
+let box = document.querySelector(".box");
+box.style.display = "none";
+
+turnOn.addEventListener("click", function () {
+  if (box.style.display == "none") {
+    box.style.display = "flex";
+  } else {
+    box.style.display = "none";
+  }
+});
+
+// TurnOff
+let turnOff = document.getElementById("turnOff");
+turnOff.addEventListener("click", function () {
+  if (box.style.display != "none") {
+    box.style.display = "none";
+  }
+});
+
+// get value from input element
+let input = document.querySelector(".search input");
+let searchBtn = document.querySelector(".search button");
+
+searchBtn.addEventListener("click", function () {
+  console.log(input.value);
+  input.value = "";
+});
